5a53c6b9cb65d300c66e697380698493f261b581
Rodrigo Romano <rodrigo@rodrigoromano.net>
2017-10-06 15:28:01 -0300

    base solution

---
+++ SharePointHostedSPA/Scripts/custom/App.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+  var getParameterByName = function (name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)', 'i').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  };
+
+  // get the HostWeb URL, title & logo image
+  var hostWebUrl = decodeURIComponent(getParameterByName("SPHostUrl"));
+  var hostWebTitle = decodeURIComponent(getParameterByName("SPHostTitle"));
+  var hostWebLogoUrl = decodeURIComponent(getParameterByName("SPHostLogoUrl"));
+
+  console.log(hostWebUrl, hostWebTitle, hostWebLogoUrl);
+
+  // create chrome control settings
+  var options = {
+    siteUrl: hostWebUrl,
+    siteTitle: hostWebTitle,
+    appIconUrl: hostWebLogoUrl,
+    appTitle: "SPA Sample",
+    settingsLinks: [
+      {
+        linkUrl: "../_layouts/mcontent.aspx",
+        displayName: "Lists Administration"
+      }
+    ]
+  };
+
+  // create the chrome control
+  var nav = new SP.UI.Controls.Navigation("chrome_ctrl_container", options);
+
+  // show chrome control
+  nav.setVisible(true);
+
+  // hide top app chrome (image & app name)
+  nav.setBottomHeaderVisible(false);
+});
